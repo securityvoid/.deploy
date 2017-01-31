9fe5e78c0867033ad6d6b6a3f71c9276b8ecb71b
Douglas Logan <dlogan@cyberninjas.com>
2017-01-30 22:37:05 -0500

    Added git commits for moving over to master.

--- makeMaster.js
+++ makeMaster.js
@@ -4,8 +4,9 @@ const fs = require('fs-extra');
 const path = require('path');
 const q = require("q");
 const exclude = ['dist', "node_modules", "deploy.js", ".idea", 'makeMaster.js', 'package.json'];
+const exec = require('child_process').exec;
 
-copyFiles().then(webPackIt).then(commitMaster).then(function(results){
+copyFiles().then(setBranchMaster).then(webPackIt).then(gitAddCommit).then(function(results){
     console.log("Success!");
 }).catch(function(err){
     console.log("Failed!");
@@ -84,8 +85,35 @@ function copyFiles(){
     return deferred.promise;
 }
 
-function commitMaster(){
+function setBranchMaster(){
     var deferred = q.defer();
-    deferred.resolve({success : true});
+    exec('git checkout master', {cwd: path.join(__dirname, "dist")}, function(error, stdout, stderr){
+        if (error) {
+            deferred.reject({success : false, error : error, stdout: stdout, stderr : stderr })
+        }
+        deferred.resolve({success : true, error : error, stdout: stdout, stderr : stderr })
+    });
+    return deferred.promise;
+}
+
+function gitAddCommit(){
+    var deferred = q.defer();
+    exec('git add .', {cwd: path.join(__dirname, "dist")}, function(error, stdout, stderr){
+        if (error) {
+            deferred.reject({success : false, error : error, stdout: stdout, stderr : stderr })
+        }
+        exec('git commit --message="Build Master:' + new Date().toISOString() + '"',
+            {cwd: path.join(__dirname, "dist")}, function(error2, stdout2, stderr2){
+            if (error) {
+                deferred.reject({success : false, error : error2, stdout: stdout2, stderr : stderr2 })
+            }
+            exec('git push', {cwd: path.join(__dirname, "dist")}, function(error3, stdout3, stderr3){
+                    if (error) {
+                        deferred.reject({success : false, error : error3, stdout: stdout3, stderr : stderr3 })
+                    }
+                    deferred.resolve({success : true, error : error3, stdout: stdout3, stderr : stderr3 })
+                });
+        });
+    });
     return deferred.promise;
 }
