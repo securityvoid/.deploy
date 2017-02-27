3b35e9ccf02156fc887f26435125f660e317d127
Douglas Logan <dlogan@cyberninjas.com>
2017-02-27 18:20:16 -0500

    Manual Copy.

--- deploy.cmd
+++ deploy.cmd
@@ -1,13 +1,13 @@
 @if "%SCM_TRACE_LEVEL%" NEQ "4" @echo off
-@echo Started: %date% %time%
+
 :: ----------------------
 :: KUDU Deployment Script
-:: Version: 1.0.10
+:: Version: 1.0.12
 :: ----------------------
 
 :: Prerequisites
 :: -------------
-@echo Start Pre-Reqs: %date% %time%
+
 :: Verify node.js installed
 where node 2>nul >nul
 IF %ERRORLEVEL% NEQ 0 (
@@ -17,19 +17,73 @@ IF %ERRORLEVEL% NEQ 0 (
 
 :: Setup
 :: -----
-@echo Start Setup: %date% %time%
+
 setlocal enabledelayedexpansion
 
+SET ARTIFACTS=%~dp0%..\artifacts
+
 IF NOT DEFINED DEPLOYMENT_SOURCE (
-  SET DEPLOYMENT_SOURCE=%~dp0%../
+  SET DEPLOYMENT_SOURCE=%~dp0%.
 )
-echo Deployment Source: %DEPLOYMENT_SOURCE%
 
 IF NOT DEFINED DEPLOYMENT_TARGET (
   SET DEPLOYMENT_TARGET=%ARTIFACTS%\wwwroot
 )
-echo Deployment Target: %DEPLOYMENT_TARGET%
 
+IF NOT DEFINED NEXT_MANIFEST_PATH (
+  SET NEXT_MANIFEST_PATH=%ARTIFACTS%\manifest
+
+  IF NOT DEFINED PREVIOUS_MANIFEST_PATH (
+    SET PREVIOUS_MANIFEST_PATH=%ARTIFACTS%\manifest
+  )
+)
+
+IF NOT DEFINED KUDU_SYNC_CMD (
+  :: Install kudu sync
+  echo Installing Kudu Sync
+  call npm install kudusync -g --silent
+  IF !ERRORLEVEL! NEQ 0 goto error
+
+  :: Locally just running "kuduSync" would also work
+  SET KUDU_SYNC_CMD=%appdata%\npm\kuduSync.cmd
+)
+
+for /F "tokens=5 delims=.\" %%a in ("%PREVIOUS_MANIFEST_PATH%") do SET PREVIOUS_SCM_COMMIT_ID=%%a
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Pre-Deployment
+:: ----------
+
+for /F %%f in ('git.exe diff --name-only %PREVIOUS_SCM_COMMIT_ID% %SCM_COMMIT_ID% ^| grep package.json') do (
+    SET PACKAGE_JSON=%%f
+    echo "NPM Install: %DEPLOYMENT_SOURCE%\%PACKAGE_JSON:~-12%"
+    pushd "%DEPLOYMENT_SOURCE%\%PACKAGE_JSON:~-12%"
+    npm install --production --progress=false --cache-min=432000
+    npm install --save json-loader --progress=false --cache-min=432000
+    IF !ERRORLEVEL! NEQ 0 goto error
+    popd
+)
+
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Deployment
+:: ----------
+
+echo Handling function App deployment.
+
+:: 1. Build Script
+node .deploy/deploy.js
+
+:: 2. Copy Files
+FOR /f "tokens=1,2" %%I IN ('git.exe diff --name-status %PREVIOUS_SCM_COMMIT_ID% %SCM_COMMIT_ID%') DO (
+  IF "%%~I" == "D" (
+    echo "REMOVING:%DEPLOYMENT_TARGET%\%%~J"
+    del "%DEPLOYMENT_TARGET%\%%~J"
+  ) ELSE (
+    echo "COPYING:%DEPLOYMENT_TARGET%\%%~J"
+    copy "%DEPLOYMENT_DIST%\%%~J" "%DEPLOYMENT_TARGET%\%%~J"
+  )
+)
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 goto end
 
@@ -56,4 +110,3 @@ exit /b 1
 :end
 endlocal
 echo Finished successfully.
-@echo Finished: %date% %time%
